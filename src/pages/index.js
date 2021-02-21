6c0e1a631f3a9993a2c98868aa7c8b0a2c7733a2
Zakaria Zerhouni <ztzerhouni@gmail.com>
2021-02-21 12:10:38 -0500

    added form

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,29 @@ export default function Home() {
     <h3>Let's try some basic HTML</h3>
     <p>Click on the image below.</p>
     <a href="https://gundam.fandom.com/wiki/Char_Aznable"><img src={CharImg} alt="" /></a>
+
+    <form method="get">
+
+      <label for="fname">First Name: </label>
+      <input type="text" id="fname" name="firstname" placeholder="First Name">
+
+      <label for="lname">First Name: </label>
+      <input type="text" id="lname" name="lastname" placeholder="Last Name">
+      <p></p>
+
+      <label for="uemail"></label>
+      <input type="email" id="uemail" name="useremail" placeholder="user@email.com">
+
+      <label for="upass"></label>
+      <input type="password" id="upass" name="userpassword" placeholder="Enter Password">
+      <p></p>
+
+      <input type="submit" name="usersubmission" value="Submit">
+
+    </form>
+
   </div>
+
+  
   );
 }
